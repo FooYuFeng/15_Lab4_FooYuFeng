58cb2b7a4b435c32d8d5db2bd0d239ef6c9a0cd2
SoleSaken <yufeng132@gmail.com>
2023-01-12 02:57:36 +0800

    Version 2
    2

--- 15_Lab4_FooYuFeng/Assets/Scripts/BasketMovementScript.cs
+++ 15_Lab4_FooYuFeng/Assets/Scripts/BasketMovementScript.cs
@@ -5,28 +5,27 @@ using UnityEngine;
 public class BasketMovementScript : MonoBehaviour
 {
     public float speed;
-   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
 
-      float horizontalInput = Input.GetAxis("Horizontal");
+        transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
 
-      transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
-
-        
-       
-
+        if (transform.position.x > 7)
+        {
+            transform.position = new Vector3(7, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -7)
+        {
+            transform.position = new Vector3(-7, transform.position.y, transform.position.z);
+        }
     }
-
-
-
-    
-
 }
